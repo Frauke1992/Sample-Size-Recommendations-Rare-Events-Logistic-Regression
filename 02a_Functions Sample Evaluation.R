bd2b43965d85817bcfd7bb87bbe39d822fc3d5c2
FlorianScharf <florian@scharf-net.de>
2024-06-25 11:08:35 +0200

    Add a function to fit GBMs to a sample

--- 02a_Functions Sample Evaluation.R
+++ 02a_Functions Sample Evaluation.R
@@ -4,6 +4,7 @@ library(glmnet) # used for elastic net
 library(dplyr)
 library(pROC) # to calculate AUC
 library(ModelMetrics) # to calculate LogLoss
+library(gbm) # for gradient boosting machine
 
 ####################################################
 ###### Function for LR without regularization ######
@@ -277,6 +278,177 @@ output.enet <- function(train_data, validation_data, upsampling, summarytype,
   return(results_enet)
 }
 
+
+##################################################
+###### Function for Gradien Boosting Machine #####
+##################################################
+
+output.gbm <- function(train_data, validation_data, upsampling, summarytype, 
+                       metrictype){
+  
+  train_data_caret <- train_data
+  # change y to factor
+  train_data_caret$Y <- as.factor(train_data_caret$Y)
+  validation_data_factor <- as.factor(validation_data$Y)
+  
+  # set factor levels
+  levels(train_data_caret$Y) <- c("ZERO","ONE")
+  levels(validation_data_factor) <- c("ZERO","ONE")
+  
+  grid_gbm <- expand.grid(
+    interaction.depth = c(1,2,3), # tree depth (= max_depth in xgboost)
+    n.minobsinnode = c(5, 10),    # end node size (= min_child_weight in xgboost)
+    n.trees = c(50,100,150),      # max number of trees (= nTrees in xgboost)
+    shrinkage = seq(.051, .201, .05)) # shrinkage/learning rate (= eta in xgboost)
+  
+  trainControl_gbm <- trainControl(method = "cv", # cross validation
+                                   # number of folds for cross validation
+                                   number = 10,
+                                   # class probabilities are computed and saved
+                                   classProbs = TRUE,
+                                   # all prediction values are saved
+                                   savePredictions = TRUE,
+                                   returnData = TRUE, # saves the data
+                                   # specified summarytype given to the function
+                                   summaryFunction = summarytype,
+                                   # upsampling to resolve class imbalance or none
+                                   sampling = upsampling)
+  
+  # train model for area under ROC curve
+  fit_gbm <- train(Y ~ .,# any given x and its two-way interactions with other
+                   # potential predictors can go into the model
+                   data = train_data_caret,
+                   # trainControl object containing the specifications above
+                   trControl = trainControl_gbm,
+                   method = "gbm",
+                   metric = metrictype,# specified performance metric
+                   tuneGrid = grid_gbm) # set tunelength to 20
+  
+  #### Analysis of training data ####
+  # extract predicted probabilities in training sample
+  predicted_gbm <- subset(fit_gbm$pred, # choose the predicted values
+                          n.trees == fit_gbm$bestTune$n.trees & #for the best alpha
+                            interaction.depth == fit_gbm$bestTune$interaction.depth &
+                            shrinkage == fit_gbm$bestTune$shrinkage &
+                            n.minobsinnode == fit_gbm$bestTune$n.minobsinnode) # and the best lambda
+  
+  
+  predicted_sorted <- arrange(predicted_gbm, rowIndex)
+  # sort data according to rowIndex in order to have rows match the rows of the
+  # original data
+  
+  predicted_prob_train_gbm <- predicted_sorted$ONE # select only the probabilities
+  
+  # assign the predicted probabilities to 0 and 1 class according to a
+  # threshold of 0.5
+  predicted_class_train_gbm <- round(predicted_prob_train_gbm)
+  if(metrictype == "ROC"){
+    auc_train_gbm <- subset(fit_gbm$results,
+                            n.trees == fit_gbm$bestTune$n.trees & #for the best alpha
+                              interaction.depth == fit_gbm$bestTune$interaction.depth &
+                              shrinkage == fit_gbm$bestTune$shrinkage &
+                              n.minobsinnode == fit_gbm$bestTune$n.minobsinnode)$ROC
+    
+    
+    # calculate logloss #ich frag mich ob hier noch n Logikfehler ist, da das hier ja mit allen
+    #berechnet wird und nicht nur mit dem subset des besten grids?
+    logloss_train_gbm_calc <- mlogLoss(fit_gbm$pred$obs,
+                                       fit_gbm$pred[c( "ZERO", "ONE")])
+    logloss_train_gbm <- NA
+    
+    
+  } else if(metrictype == "logLoss"){
+    # extract logloss
+    logloss_train_gbm <- subset(fit_gbm$results,
+                                n.trees == fit_gbm$bestTune$n.trees & #for the best alpha
+                                  interaction.depth == fit_gbm$bestTune$interaction.depth &
+                                  shrinkage == fit_gbm$bestTune$shrinkage &
+                                  n.minobsinnode == fit_gbm$bestTune$n.minobsinnode)$logLoss
+    # calculate logloss
+    logloss_train_gbm_calc <- mlogLoss(fit_gbm$pred$obs,
+                                       fit_gbm$pred[c( "ZERO", "ONE")])
+    # calculate AUC
+    auc_train_gbm <- auc(train_data$Y, predicted_prob_train_gbm)
+  }
+  
+  
+  # calculate misclassification error
+  misclassification_train_gbm <- mean(predicted_class_train_gbm !=
+                                        train_data$Y)
+  
+  
+  # create confusion matrix
+  confusion_train_gbm <- as.matrix(confusionMatrix.train(
+    data = fit_gbm,
+    norm = "none",
+    positive = 1)$table)
+  
+  
+  #### Analysis of Validation data ####
+  # Extract variable importances from gbm
+  varImp_gbm <- varImp(fit_gbm)
+  
+  # get predicted probabilities
+  predicted_prob_validation_gbm <- predict(fit_gbm, validation_data[-1],
+                                           type = "prob" )
+  
+  # assign the predicted probabilities to 0 and 1 class according to
+  # threshold of 0.5
+  predicted_class_validation_gbm <- round(predicted_prob_validation_gbm[,2])
+  
+  # calculate AUC
+  auc_validation_gbm <- auc(validation_data$Y,
+                            predicted_prob_validation_gbm[,2])
+  
+  # calculate misclassification error
+  misclassification_validation_gbm <- mean(predicted_class_validation_gbm
+                                           != validation_data$Y)
+  
+  # calculate logloss
+  logloss_validation_gbm_calc <- mlogLoss(validation_data_factor,
+                                          predicted_prob_validation_gbm)
+  logloss_validation_gbm <- NA
+  
+  # create confusion matrix
+  confusion_validation_gbm <- as.matrix(caret::confusionMatrix(
+    data = as.factor(predicted_class_validation_gbm),
+    reference = as.factor(validation_data$Y),
+    positive = "1")$table)
+  
+  #### Create Output ####
+  # bind performance metrics in matrix
+  performance_metrics_gbm <- rbind(c(auc_train_gbm,
+                                     misclassification_train_gbm,
+                                     logloss_train_gbm,
+                                     logloss_train_gbm_calc),
+                                   c(auc_validation_gbm,
+                                     misclassification_validation_gbm,
+                                     logloss_validation_gbm,
+                                     logloss_validation_gbm_calc))
+  # name columns
+  colnames(performance_metrics_gbm) <- c("auc", "misclassification",
+                                         "logLossModel", "logLossCalculated")
+  # name rows
+  rownames(performance_metrics_gbm) <- c("train", "validation")
+  
+  # create list with outputs
+  results_gbm <- list(varImp_gbm$importance,
+                      predicted_prob_validation_gbm[2],
+                      performance_metrics_gbm,
+                      confusion_train_gbm,
+                      confusion_validation_gbm,
+                      fit_gbm$bestTune)
+  # name sublists
+  names(results_gbm) <- c("variableImportances", "predictedProbabilities",
+                          "performanceMetrics", "confusionMatrixTraining",
+                          "confusionMatrixvalidation",
+                          "TuningParameters")
+  
+  return(results_gbm)
+}
+
+
+
 ##########################################################################
 ##### Function for analysis and error handling for LR and ElasticNet #####
 ##########################################################################
@@ -435,4 +607,3 @@ results.caret <- function(train_data, validation_data, samplingtype){
   return(results)
   
 }
-
